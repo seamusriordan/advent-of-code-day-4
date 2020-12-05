8ded529d0836a4bdd6b36761d5f0117be62f01fc
Seamus Riordan <seamus@riordan.io>
2020-12-05 13:58:21 -0600

    Match end of line as well for hair color

--- src/test/kotlin/MainTests.kt
+++ src/test/kotlin/MainTests.kt
@@ -216,4 +216,41 @@ class MainTests {
 
         assertEquals(2, countValidPassports(input))
     }
+
+    @Test
+    fun `Example invalid input`() {
+        val input = "eyr:1972 cid:100\r\n" +
+                "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\r\n" +
+                "\r\n" +
+                "iyr:2019\r\n" +
+                "hcl:#602927 eyr:1967 hgt:170cm\r\n" +
+                "ecl:grn pid:012533040 byr:1946\r\n" +
+                "\r\n" +
+                "hcl:dab227 iyr:2012\r\n" +
+                "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\r\n" +
+                "\r\n" +
+                "hgt:59cm ecl:zzz\r\n" +
+                "eyr:2038 hcl:74454a iyr:2023\r\n" +
+                "pid:3556412378 byr:2007"
+
+        assertEquals(0, countValidPassports(input))
+    }
+
+    @Test
+    fun `Example valid input`() {
+        val input = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\r\n" +
+                "hcl:#623a2f\r\n" +
+                "\r\n" +
+                "eyr:2029 ecl:blu cid:129 byr:1989\r\n" +
+                "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\r\n" +
+                "\r\n" +
+                "hcl:#888785\r\n" +
+                "hgt:164cm byr:2001 iyr:2015 cid:88\r\n" +
+                "pid:545766238 ecl:hzl\r\n" +
+                "eyr:2022\r\n" +
+                "\r\n" +
+                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+
+        assertEquals(4, countValidPassports(input))
+    }
 }
