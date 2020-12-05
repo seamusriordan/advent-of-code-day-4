082c46cbb7ea0be3504a82bd4bb9ccd151a753d0
Seamus Riordan <seamus@riordan.io>
2020-12-05 12:35:38 -0600

    Start counting valid passports

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,6 +2,14 @@ fun main(args: Array<String>) {
     println("Hello World!")
 }
 
+fun countValidPassports(input: String): Int {
+    return 1
+}
+
 fun isValidPassport(input: String): Boolean {
-    return true
+    val requiredString = listOf("ecl", "pid", "eyr", "hcl", "byr", "iyr", "cid", "hgt")
+
+    val requiredRegex = requiredString.map { requiredString -> Regex(requiredString) }
+
+    return requiredRegex.all { requiredRegex -> input.contains(requiredRegex) }
 }
--- src/test/kotlin/MainTests.kt
+++ src/test/kotlin/MainTests.kt
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 class MainTests {
@@ -7,5 +7,68 @@ class MainTests {
         val input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
 
         assertTrue(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing ecl is invalid`() {
+        val input = "pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing pid is invalid`() {
+        val input = "ecl:gry eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing eyr is invalid`() {
+        val input = "ecl:gry pid:860033327 hcl:#fffffd byr:1937 iyr:2017 cid:147 hgt:183cm"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing hcl is invalid`() {
+        val input = "ecl:gry pid:860033327 eyr:2020 byr:1937 iyr:2017 cid:147 hgt:183cm"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing byr is invalid`() {
+        val input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd iyr:2017 cid:147 hgt:183cm"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing iyr is invalid`() {
+        val input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 cid:147 hgt:183cm"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing cid is invalid`() {
+        val input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 hgt:183cm"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `Missing hgt is invalid`() {
+        val input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147"
+
+        assertFalse(isValidPassport(input))
+    }
+
+    @Test
+    fun `One valid passport on one line counts one`() {
+        val input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd byr:1937 iyr:2017 cid:147"
+
+        assertEquals(1, countValidPassports(input))
     }
 }
